b16c0e9b30752c107406d6ef229d1a42751ff512
sbhalla98 <sonalibhalla98@gmail.com>
2020-01-19 20:32:51 +0530

    time and work questions added

--- index.js
+++ index.js
@@ -1,32 +1,155 @@
 questions =[{
-    question : "1. How many kilogram of sugar costing Rs. 9 per kg must be mixed with 27 kg of sugar costing Rs. 7 per kg so that there may be a gain of 10% by selling the mixture at Rs. 9.24 per kg?",
-    option1 : "3",
-    option2 :"4",
-    option3:"5",
-    option4:"6",
-    ans:"7fewugfhidjkdcwfvgdsahbjsahjsads<div>adchkjnldk</div>"
+    question : "1. A and B together can do a specific work in 8 days. B alone can do it in 10 days, then time taken by A alone is?",
+    option1 : "A. 28 days",
+    option2 :"B. 36 days",
+    option3:"C. 40 days",
+    option4:"D. 32 days",
+    ans:"<div>C</div>"
 },{
-    question : "1. How many kilogram of sugar costing Rs. 9 per kg must be mixed with 27 kg of sugar costing Rs. 7 per kg so that there may be a gain of 10% by selling the mixture at Rs. 9.24 per kg?",
-    option1 : "3",
-    option2 :"4",
-    option3:"5",
-    option4:"6",
-    ans:"7"
+    question : "2. A, B, C together can do a work in 6 days. A alone can do it in 12 days while B alone can do it in 18 days, then time taken by C is?",
+    option1 : "A. 9 days",
+    option2 :"B. 18 days",
+    option3:"C. 27 days",
+    option4:"D. 36 days",
+    ans:"<div>D</div>"
 },{
-    question : "1. How many kilogram of sugar costing Rs. 9 per kg must be mixed with 27 kg of sugar costing Rs. 7 per kg so that there may be a gain of 10% by selling the mixture at Rs. 9.24 per kg?",
-    option1 : "3",
-    option2 :"4",
-    option3:"5",
-    option4:"6",
-    ans:"7"
+    question : "3. A & B working together can do a piece of work in 12 days. B & C working together can do a piece of work in 15 days. C & A working together can do a piece of work in 20 days. In how many days A can do the same work?",
+    option1 : "20",
+    option2 :"30",
+    option3:"40",
+    option4:"60",
+    ans:"<div>B</div>"
 },{
-    question : "1. How many kilogram of sugar costing Rs. 9 per kg must be mixed with 27 kg of sugar costing Rs. 7 per kg so that there may be a gain of 10% by selling the mixture at Rs. 9.24 per kg?",
+    question : "4. A and B can do a piece of work in 15 days. B and C can do the same work in 10 days, A and C can do the same work in 12 days. Time taken by A, B and C together to do the job is?",
+    option1 : "A. 4 days",
+    option2 :"B. 9 days",
+    option3:"C. 8 days",
+    option4:"D. 5 days",
+    ans:"<div>C</div>"
+},{
+    question : "5. A & B working together can do a piece of work in 8 days. B & C working together can do a piece of work in 12 days. A, B and C all working together can do a piece of work in 6 days. In how many days A & C working together can do?",
     option1 : "3",
     option2 :"4",
-    option3:"5",
-    option4:"6",
-    ans:"7"
-}]
+    option3:"6",
+    option4:"8",
+    ans:"<div>D</div>"
+},
+{
+    question : "6. A can do a piece of work in 12 days. B can do same piece of work in 15 days. After A had worked for 3 days B also join A to finish the remaining work. In how many days work will be finished?",
+    option1 : "3",
+    option2 :"5",
+    option3:"6",
+    option4:"8",
+    ans:"<div>D</div>"
+},
+{
+    question : "7. A can do a piece of work in 9 days. B can do same piece of work in 10 days. C can do same piece of work in 15 days. B and C start working and left after 2 days. In how many days remaining work will be finished by A?                                                                                                          ",
+    option1 : "4",
+    option2 :"6",
+    option3:"8",
+    option4:"10",
+    ans:"<div>B</div>"
+},
+{
+    question : "8. A can do a piece of work in 25 days and B in 20 days. They work together for 5 days and then A goes away. In how many days will B finish the remaining work? ",
+    option1 : "17",
+    option2 :"11",
+    option3:"12",
+    option4:"10",
+    ans:"<div>B</div>"
+},
+{
+    question : "9. A can do a piece of work in 8 hours. B can do same piece of work in 12 hours. A starts working at 9AM and they worked on alternative hours. At which time work will be finished?",
+    option1 : "3:30 pm",
+    option2 :"6:30 pm",
+    option3:"3:30 am",
+    option4:"6:30 am",
+    ans:"<div>B</div>"
+},
+{
+    question : "10. A, B and C can do a piece of work in 11, 20 and 55 days respectively. In how many days work will be finished if A is assisted by B and C on alternative day?",
+    option1 : "4",
+    option2 :"6",
+    option3:"8",
+    option4:"16",
+    ans:"<div>C</div>"
+},
+{
+    question : "11. A, B and C can do a piece of work in 20, 30 and 60 days respectively. In how many days work will be finished by A, if he is assisted by B and C on every third day?",
+    option1 : "5",
+    option2 :"10",
+    option3:"15",
+    option4:"20",
+    ans:"<div>C</div>"
+},
+{
+    question : "12. A can do a work in same time in which B & C can do it working together. A & B working together can do it in 10 days. C can do same in 50 days. In how many days B can do the work individually?",
+    option1 : "2",
+    option2 :"5",
+    option3:"15",
+    option4:"25",
+    ans:"<div>D</div>"
+},
+{
+    question : " 13. A and B can do a piece of work in 20 and 30 days respectively. Both starts working on same time but B left the work 5 days before the completion of work. In how many days work will complete?",
+    option1 : "12",
+    option2 :"14",
+    option3:"16",
+    option4:"20",
+    ans:"<div>B</div>"
+},
+{
+    question : "14. A, B and C can do a piece of work in 24, 32 and 64 days respectively. They starts working, A left the work after 6 days while B left the work before 6 days from the completion of work. In how many days work will be finished?",
+    option1 : "20",
+    option2 :"18",
+    option3:"15",
+    option4:"None of these",
+    ans:"<div>A</div>"
+},{
+    question : "15. A & B working together can do a job in 30 days. They worked only for 20 days and the rest job is done by A in next 20 days. In how many days A can do the complete job individually?",
+    option1 : "30",
+    option2 :"40",
+    option3:"60",
+    option4:"120",
+    ans:"<div>C</div>"
+},{
+    question : "16. A & B working together can do a piece of work in 12 days. B & C working together can do same work in 16 days. A worked for 5 days, B for 7 days and rest work is finished by C in 13 days. In how many days working alone C can do the same work?",
+    option1 : "48",
+    option2 :"24",
+    option3:"18",
+    option4:"12",
+    ans:"<div>B</div>"
+},{
+    question : "17. A is twice as good as B and together they can finish the work in 18 days. In how many days A will finish the same work?",
+    option1 : "9",
+    option2 :"24",
+    option3:"54",
+    option4:"27",
+    ans:"<div>D</div>"
+},{
+    question : "18. Monica can do a job in 20 days. Tanya is 25% more efficient than Monica. In how many days Tanya will finish the same work?",
+    option1 : "14",
+    option2 :"15",
+    option3:"16",
+    option4:"18",
+    ans:"<div>C</div>"
+},{
+    question : "19. A is 50% more efficient than B. C does half of the work done by A & B together. If C alone do the work in 40 days. In how many days all will finish the same work together?",
+    option1 : "10/3",
+    option2 :"20/3",
+    option3:"30",
+    option4:"40/3",
+    ans:"<div>D</div>"
+},{
+    question : "20. A is thrice as good as B and he is able to finish the work 60 days less than B. In how many days they will finish the same work together?",
+    option1 : "90/4",
+    option2 :"45/4",
+    option3:"30/4",
+    option4:"22/4",
+    ans:"<div>A</div>"
+},]
+
+
 
 onload= f1;
 function f1(){
